6b537f56adb6de9395f15dabc363fda92f0f3692
JohnGammie <gammie@y7mail.com>
2025-06-25 23:14:05 +1000

    Add content added on page load.

---
+++ src/pageLoad.js
@@ -0,0 +1,11 @@
+export function pageLoad() {
+    let divContent = document.querySelector("#content");
+
+    let restaurantName = document.createElement("h1");
+    restaurantName.textContent = "Steve's Bistro!";
+    divContent.appendChild(restaurantName);
+
+    let restaurantDescription = document.createElement("p");
+    restaurantDescription.textContent = "The best food ever.";
+    divContent.appendChild(restaurantDescription);
+}
